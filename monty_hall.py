67a563816fdd94e5fa8a31e94d05c3e2e5e2229d
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2019-02-18 14:41:16 -0600

    ran through flake 8
    changed as needed

--- monty_hall.py
+++ monty_hall.py
@@ -1,26 +1,35 @@
-"""
-Program that tests the monty hall problem.
-"""
-
 from __future__ import division
 import random
 import time
 
-# contestant sticks with one door
+
+"""
+this program tests the monty hall problem
+"""
+
+
 def pick_one_door(runtime):
-    # defines default values
-    # not sure if anyone needs to record the winning doors
-    #win_doors = []
+    """
+    in this scenario, the contestant will pick one door, and will not
+    change their mind given the chance.
+    """
+    # not sure if anyone wants to record the winning doors, leaving in
+    # as an option to uncomment.
+    # #win_doors = []
+    # create some empty lists for later
     choices = []
     wins = []
+    # mark the time
     then = time.time()
+    # initialize the now var
     now = 0
     while now <= runtime:
+        # now is the current time minus the then var.
         now = time.time()-then
         # choose winning door
         winning_door = random.choice('abc')
         # not sure if anyone needs to record the winning doors
-        #win_doors.append(winning_door)
+        # #win_doors.append(winning_door)
         # contestant chooses door
         choice = random.choice('abc')
         # record choice
@@ -31,22 +40,30 @@ def pick_one_door(runtime):
             wins.append(choice)
     return wins, choices
 
-# contestant is shown whats behind another door, and decides
-# to pick another door
+
 def pick_two_doors(runtime):
-    #win_doors = []
+    """
+    here, the contestant selects a door but is given a chance to pick
+    another, after being shown whats behind a non-winning door. they
+    will then select the remaining door
+    """
+    # #win_doors = []
+    # empty lists for later
     first_choices = []
     choices = []
     wins = []
     three_doors = ['a', 'b', 'c']
+    # mark the time
     then = time.time()
+    # initialize the now var
     now = 0
     while now <= runtime:
         now = time.time()-then
-        # copies the three door list
+        # copies the three door list since well be removing values
         two_doors = three_doors[:]
         winning_door = random.choice('abc')
-        #win_doors.append(winning_door)
+        # we used to record the winning door list, keeping for now
+        # #win_doors.append(winning_door)
         # determines which two doors have goats behind them
         # if the goat thing doesnt make sense google monty hall problem
         if winning_door == 'a':
@@ -64,8 +81,7 @@ def pick_two_doors(runtime):
         if choice == goat1:
             first_pick = choice
             show_goat = goat2
-            # picker wont pick the shown goat and has to
-            # change doors
+            # picker wont pick the shown goat and has to change doors
             two_doors.remove(show_goat)
             two_doors.remove(first_pick)
             # picks the only element left in the array
@@ -89,20 +105,21 @@ def pick_two_doors(runtime):
             wins.append(modified_choice)
     return wins, choices
 
+
 def do_maths(wins, choices, type):
     # defines default values
     a_picked = choices.count('a')
     b_picked = choices.count('b')
     c_picked = choices.count('c')
-    a_percent = format((a_picked / len(choices)) * 100, '.4f' )
-    b_percent = format((b_picked / len(choices)) * 100, '.4f' )
-    c_percent = format((c_picked / len(choices)) * 100, '.4f' )
+    a_percent = format((a_picked / len(choices)) * 100, '.4f')
+    b_percent = format((b_picked / len(choices)) * 100, '.4f')
+    c_percent = format((c_picked / len(choices)) * 100, '.4f')
     a_won = wins.count('a')
     b_won = wins.count('b')
     c_won = wins.count('c')
-    a_win_percent = format((a_won / len(wins)) * 100, '.4f' )
-    b_win_percent = format((b_won / len(wins)) * 100, '.4f' )
-    c_win_percent = format((c_won / len(wins)) * 100, '.4f' )
+    a_win_percent = format((a_won / len(wins)) * 100, '.4f')
+    b_win_percent = format((b_won / len(wins)) * 100, '.4f')
+    c_win_percent = format((c_won / len(wins)) * 100, '.4f')
     win_avg = format((len(wins) / len(choices)) * 100, '.4f')
     # displays results
     if type > 1:
@@ -117,7 +134,7 @@ def do_maths(wins, choices, type):
 
 
 def main():
-    ## simple greeting
+    # simple greeting
     print("Welcome to the Monty Hall Problem!".center(80, '/'))
     # begins timer
     start = time.time()
@@ -127,6 +144,7 @@ def main():
     do_maths(w, c, 2)
     end = str(round(time.time()-start))
     print('\nran for:  ' + end + ' seconds\n' + '...fin')
+
 
 if __name__ == '__main__':
     main()
